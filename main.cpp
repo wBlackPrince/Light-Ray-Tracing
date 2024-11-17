1870d5245b62fbc7be6571f37f7149178fe6c4c0
Eduard Nikitin <102594709+wBlackPrince@users.noreply.github.com>
2024-11-17 13:45:32 +0300

    i added sphere normals

--- main.cpp
+++ main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
-#include <cmath>
 
 #include "color.h"
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const point3& center, double radius, ray& r) {
+double hit_sphere(const point3& center, double radius, ray& r) {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    
+    if (discriminant < 0){
+        return -1;
+    }
+    else{
+        return ((-b - std::sqrt(discriminant)) / (2.0 * a));
+    }
 }
 
+
 color ray_color(ray& r){
-    if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0); 
